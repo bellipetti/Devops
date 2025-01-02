29b8faa59cbfdde542d3eb4082d7791bc189b0d4
bellipetti <nkjok7@gmail.com>
2025-01-02 17:16:21 +0900

    Add files via upload

---
+++ springBoot/2024.12.26_Dependency Injection/Hello.java
@@ -0,0 +1,9 @@
+// Hello.java
+package com.example.testapp.domain;
+
+public class Hello {
+
+    public String sayHello() {
+        return "hello!!";
+    }
+ }
---
+++ springBoot/2024.12.26_Dependency Injection/Sender.java
@@ -0,0 +1,7 @@
+package com.example.testapp.domain;
+
+public interface Sender {
+    // public abstract 생략
+    void sendText(String username, String message); 
+    
+}
