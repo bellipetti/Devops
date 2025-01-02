5ccbfb837aff21ff09ffbc780c73c75fc4227b79
bellipetti <nkjok7@gmail.com>
2025-01-02 17:15:37 +0900

    Add files via upload

---
+++ springBoot/2024.12.26_befor/TestappApplicationTests.java
@@ -0,0 +1,14 @@
+// TestappApplicationTests.java
+package com.example1.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
