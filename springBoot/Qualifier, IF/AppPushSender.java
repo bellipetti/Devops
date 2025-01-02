3c6ed68c7462a79c3ba9e0c67d1f6b4df963be73
bellipetti <nkjok7@gmail.com>
2025-01-02 17:04:53 +0900

    Delete springBoot/Qualifier, IF directory

--- springBoot/Qualifier, IF/AppPushSender.java
+++
@@ -1,9 +0,0 @@
-package com.example1.testapp.domain;
-
-public class AppPushSender implements Sender {
-
-    @Override
-    public void sendText(String username, String message) {
-    System.out.println("App Push " + username + ":" + message);
-    }
-}
--- springBoot/Qualifier, IF/Sender.java
+++
@@ -1,7 +0,0 @@
-package com.example1.testapp.domain;
-
-public interface Sender {
-    // public abstract 생략
-    void sendText(String username, String message); 
-    
-}
